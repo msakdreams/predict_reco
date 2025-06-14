77bf6db18ff4e0b0e7638a0784579f2beb72c5d7
MSAKabir <57796270+MSAKabir@users.noreply.github.com>
2025-06-14 20:04:57 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+import pandas as pd
+from flask import Flask, request, render_template, send_file
+from sklearn.ensemble import RandomForestRegressor
+from werkzeug.utils import secure_filename
+from flask_socketio import SocketIO
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+socketio = SocketIO(app)
+
+def predict_and_append(df):
+    X = df.iloc[:, 0:4]  # Columns 1 to 4
+    y = df.iloc[:, 4]    # Column 5 (calculated value)
+
+    model = RandomForestRegressor()
+    model.fit(X, y)
+
+    df['Predicted Output'] = model.predict(X)  # Column 7
+    return df
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'excel' not in request.files:
+            return "No file uploaded."
+
+        file = request.files['excel']
+        if file.filename == '':
+            return "Empty file name."
+
+        filename = secure_filename(file.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(path)
+
+        df = pd.read_excel(path)
+
+        if df.shape[1] < 6:
+            return "Excel file must have at least 6 columns."
+
+        result_df = predict_and_append(df)
+        output_path = os.path.join(app.config['UPLOAD_FOLDER'], 'predicted_' + filename)
+        result_df.to_excel(output_path, index=False)
+
+        return send_file(output_path, as_attachment=True)
+
+    return render_template('upload.html')
+
+
+
+
+
+@socketio.on('connect')
+
+def handle_connect():
+
+    print('Client connected')
+
+@socketio.on('disconnect')
+
+def handle_disconnect():
+
+    print('Client disconnected')
+
+if __name__ == '__main__':
+
+    socketio.run(app, debug=True)
